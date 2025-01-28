ba21155705600a44e75e680f2ab860349bc2ea1b
Josh Moskoff <jmmoskoff@gmail.com>
2025-01-28 17:15:49 -0500

    added auth for our project

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-from routers import url, analytics, redirect
+from routers import url
+# , analytics, redirect
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React app URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 app.include_router(url.router, prefix="/url", tags=["URL Shortening"])
-app.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
-app.include_router(redirect.router, tags=["Redirect"])
+# app.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
+# app.include_router(redirect.router, tags=["Redirect"])
 
 if __name__ == "__main__":
     import uvicorn
